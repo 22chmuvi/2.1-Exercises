58736d343a8c25ef924f801f76237324b1c2714d
22chmuvi <57362622+22chmuvi@users.noreply.github.com>
2020-01-14 13:29:58 -0500

    didn't finish yet

---
+++ mpg.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import csv
+
+trips = []
+
+def get_miles_driven():
+    while True:
+        miles_driven = float(input("Enter miles driven :     "))
+        if miles_driven > 0:
+            return miles_driven
+        else:
+            print("Entry must be greater than zero. Please try again.\n")
+            continue
+
+def get_gallons_used():
+    while True:
+        gallons_used = float(input("Enter gallons of gas:    "))
+        if gallons_used > 0:
+            return gallons_used
+        else:
+            print("Entry must be greater than zero. Please try again.\n")
+            continue
+
+def write_trips(data_list)
+    with open("trips.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(data_list)
+
+def main():
+    # display a welcome message
+    print("The Miles Per Gallon application")
+    print()
+
+    more = "y"
+    while more.lower() == "y":
+        miles_driven = get_miles_driven()
+        gallons_used = get_gallons_used()
+
+        mpg = round((miles_driven / gallons_used), 2)
+        print("Miles Per Gallon:\t " + str(mpg))
+        print()
+
+        data_list = [["Distance", "Gallons", "MPG"]]
+        data_list.append([miles_driven, gallons_used, mpg])
+
+        more = input("More entries? (y or n): ")
+
+    print("Bye")
+
+if __name__ == "__main__":
+    main()
